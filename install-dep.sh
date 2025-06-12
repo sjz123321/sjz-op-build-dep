ad866c0f764d7723343c8d0ccca0789bb8e46e4d
Shi Jiezuan <37840247+sjz123321@users.noreply.github.com>
2025-06-12 16:42:51 +0800

    Update install-dep.sh

--- install-dep.sh
+++ install-dep.sh
@@ -28,14 +28,3 @@ apt-get install -y libfuse-dev
 apt-get install -y rename pigz clang gnupg snapd
 snap install jq
 apt-get install -y $(curl -fsSL https://tinyurl.com/ubuntu2204-make-openwrt)
-
-# 安装gcc g++
-GCC_VERSION="13"
-add-apt-repository --yes ppa:ubuntu-toolchain-r/test
-apt-get update -y
-apt-get install -y gcc-${GCC_VERSION}
-apt-get install -y g++-${GCC_VERSION}
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-${GCC_VERSION} 60
-update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-${GCC_VERSION} 60
-update-alternatives --config gcc
-update-alternatives --config g++
